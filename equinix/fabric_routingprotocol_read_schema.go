cb74ba4ce5682c3a283658c0d9d3ffafb4f350e1
Sabrina Wong <sabwong@equinix.com>
2023-05-23 13:29:19 -0700

    add readFabricRoutingProtocolResourceSchema to read_schema.go

--- equinix/fabric_routingprotocol_read_schema.go
+++ equinix/fabric_routingprotocol_read_schema.go
@@ -84,7 +84,7 @@ func readRoutingProtocolOperationSch() map[string]*schema.Schema {
 			Computed:    true,
 			Description: "Errors occurred",
 			Elem: &schema.Resource{
-				Schema: createOperationalErrorSch(),
+				Schema: readOperationalErrorSch(),
 			},
 		},
 	}
@@ -114,4 +114,117 @@ func readRoutingProtocolChangeSch() map[string]*schema.Schema {
 	}
 }
 
-// todo: copy and rename createFabricRoutingProtocolResourceSchema() from schema.go
+func readFabricRoutingProtocolResourceSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"href": {
+			Type: 			schema.TypeString,
+			Computed: 		true,
+			Description: 	"Routing Protocol URI information",
+		},
+		"type": {
+			Type:         schema.TypeString,
+			Required:     true,
+			Description:  "Defines the routing protocol type like BGP or DIRECT",
+		},
+		"uuid": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "Equinix-assigned routing protocol identifier",
+		},
+		"name": {
+			Type:        schema.TypeString,
+			Required:    true,
+			Description: "Routing Protocol name. An alpha-numeric 24 characters string which can include only hyphens and underscores",
+		},
+		"description": {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "Customer-provided Fabric Routing Protocol description",
+		},
+		"state": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "Routing Protocol overall state",
+		},
+		"operation": {
+			Type:        schema.TypeSet,
+			Computed:    true,
+			Description: "Routing Protocol type-specific operational data",
+			Elem: &schema.Resource{
+				Schema: readRoutingProtocolOperationSch(),
+			},
+		},
+		"change": {
+			Type: schema.TypeSet,
+			Computed: true,
+			Description: "Routing Protocol configuration Changes",
+			Elem: &schema.Resource{
+				Schema: readRoutingProtocolChangeSch(),
+			},
+		},
+		// fixme: questions about primative behaviors for ipv4 and ipv6
+		"direct_ipv4": {
+			Type: schema.TypeSet,
+			Required: true,
+			Description: "Routing Protocol Direct IPv4",
+			Elem: &schema.Resource{
+				Schema: readDirectConnectionIpv4Sch(),
+			},
+		},
+		"direct_ipv6": {
+			Type: schema.TypeSet,
+			Optional: true,
+			Description: "Routing Protocol Direct IPv6",
+			Elem: &schema.Resource{
+				Schema: readDirectConnectionIpv6Sch(),
+			},
+		},
+		"bgp_ipv4": {
+			Type: schema.TypeSet,
+			Optional: true,
+			Description: "Routing Protocol BGP IPv4",
+			Elem: &schema.Resource{
+				Schema: readBgpConnectionIpv4Sch(),
+			},
+		},
+		"bgp_ipv6": {
+			Type: schema.TypeSet,
+			Optional: true,
+			Description: "Routing Protocol BGP IPv6",
+			Elem: &schema.Resource{
+				Schema: readBgpConnectionIpv6Sch(),
+			},
+		},
+		"customer_asn": {
+			Type:        schema.TypeInt,
+			Required:    true,
+			Description: "Customer-provided ASN",
+		},
+		"equinix_asn": {
+			Type:        schema.TypeInt,
+			Computed:    true,
+			Description: "Equinix ASN",
+		},
+		"bgp_auth_key": {
+			Type: schema.TypeString,
+			Optional: true,
+			Description: "BGP authorization key",
+		},
+		"bfd": {
+			Type: schema.TypeSet,
+			Optional: true,
+			Description: "Bidirectional Forwarding Detection",
+			Elem: &schema.Resource{
+				Schema: readRoutingProtocolBfdSch(),
+			},
+		},
+		"change_log": {
+			Type:        schema.TypeSet,
+			Computed:    true,
+			Description: "Captures Routing Protocol lifecycle change information",
+			Elem: &schema.Resource{
+				Schema: readChangeLogSch(),
+			},
+		},
+	}
+}
